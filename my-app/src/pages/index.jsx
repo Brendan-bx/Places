d3051dfdc6464a0655f770abf511ac78f887cd3b
Brendan Bx <Brendanbx@gmail.com>
2024-04-06 12:44:44 +0200

    Affichage resto homepage

--- my-app/src/pages/index.jsx
+++ my-app/src/pages/index.jsx
@@ -5,84 +5,18 @@ import { lieuTypes } from '@/utils/constants'
 import axios from 'axios'
 import { useState } from 'react'
 
-const products = [
-    {
-        id: 1,
-        name: 'Earthen Bottle',
-        href: '#',
-        price: '$48',
-        imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-        imageAlt:
-            'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
-    },
-    {
-        id: 2,
-        name: 'Nomad Tumbler',
-        href: '#',
-        price: '$35',
-        imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-        imageAlt:
-            'Olive drab green insulated bottle with flared screw lid and flat top.',
-    },
-    {
-        id: 3,
-        name: 'Focus Paper Refill',
-        href: '#',
-        price: '$89',
-        imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-        imageAlt:
-            'Person using a pen to cross a task off a productivity paper card.',
-    },
-    {
-        id: 4,
-        name: 'Machined Mechanical Pencil',
-        href: '#',
-        price: '$35',
-        imageSrc: 'dfgjdfgjshfglqjkdfhglksjdfhglksjdfghlkjh',
-    },
-    {
-        id: 5,
-        name: 'Machined Mechanical Pencil',
-        href: '#',
-        price: '$35',
-        imageSrc: 'dfgjdfgjshfglqjkdfhglksjdfhglksjdfghlkjh',
-    },
-    {
-        id: 5,
-        name: 'Machined Mechanical Pencil',
-        href: '#',
-        price: '$35',
-        imageSrc: 'dfgjdfgjshfglqjkdfhglksjdfhglksjdfghlkjh',
-    },
-    // More products...
-]
+export const getServerSideProps = async () => {
+    const { data: resto } = await axios(
+        'http://localhost:3000/api/places/create/resto'
+    )
 
-/*export const getServerSideProps = async () => {
-    try {
-        const response = await axios(
-            'http://localhost:3000/api/places/create/resto'
-        )
-        console.log('Response:', response)
-
-        const resto = response.data // Assuming the data returned is the object you expect
-        console.log('Resto:', resto)
-
-        return {
-            props: { initialResto: Object.values(resto) },
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error)
-        return {
-            props: { initialResto: null },
-        }
+    return {
+        props: { initialResto: Object.values(resto) },
     }
-}*/
+}
 
-export default function Home({}) {
-    const [resto, setResto] = useState([])
+export default function Home({ initialResto }) {
+    const [resto, setResto] = useState(initialResto)
 
     return (
         <>
@@ -93,38 +27,51 @@ export default function Home({}) {
                         <h2>Places</h2>
 
                         <div className="grid gap-x-8 gap-y-12 sm:grid-cols-2 grid-cols-3 xl:gap-x-8">
-                            {resto.map((name, city, postalCode) => (
-                                <a
-                                    key={name}
-                                    href={name}
-                                    className="group border-solid border-2 border-sky-500"
-                                >
-                                    <h3 className="mt-4 text-sm text-gray-700">
-                                        {city}
-                                        {postalCode}
-                                    </h3>
-                                    <p className="mt-1 text-lg font-medium text-gray-900">
-                                        {country}
-                                    </p>
-                                    <div className="flex justify-around">
-                                        <Button
-                                            variant="primary"
-                                            size="md"
-                                            className="hidden group-hover:inline"
-                                        >
-                                            INFOS
-                                        </Button>
-                                    </div>
-                                </a>
-                            ))}
+                            {resto.map(
+                                ({
+                                    _id,
+                                    lieuTypes,
+                                    name,
+                                    address,
+                                    city,
+                                    postalCode,
+                                    country,
+                                    cuisineTypes,
+                                    stars,
+                                    avgPrice,
+                                }) => (
+                                    <a
+                                        key={_id}
+                                        href={_id}
+                                        className="group border-solid border-2 border-sky-500"
+                                    >
+                                        <h3 className="mt-4 text-sm text-gray-700">
+                                            {city}--
+                                            {postalCode}
+                                        </h3>
+                                        <p className="mt-1 text-lg font-medium text-gray-900">
+                                            {country}
+                                        </p>
+                                        <div className="flex justify-around">
+                                            <Button
+                                                variant="primary"
+                                                size="md"
+                                                className="hidden group-hover:inline"
+                                            >
+                                                INFOS
+                                            </Button>
+                                        </div>
+                                    </a>
+                                )
+                            )}
                         </div>
                     </div>
                 </section>
                 <aside className="p-4 w-36 h-36">
-                    <p>Places</p>
+                    <p>Types of places</p>
                     <select className="border-solid border-2 border-gray-600">
                         {lieuTypes.map((lieuType) => (
-                            <option value={lieuType}>
+                            <option value={lieuType} key={lieuType}>
                                 {capitalizeFirstLetter(lieuType)}
                             </option>
                         ))}
