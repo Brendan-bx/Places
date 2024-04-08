03d16f614784d20f7e312dc700bbdde4c2736bf6
Brendan Bx <Brendanbx@gmail.com>
2024-04-08 19:02:23 +0200

    affichage ou non du avgPrice museum form

--- my-app/src/pages/list/create/musee.jsx
+++ my-app/src/pages/list/create/musee.jsx
@@ -25,6 +25,7 @@ import {
 } from '@/validators'
 import axios from 'axios'
 import { Formik } from 'formik'
+import { useState } from 'react'
 import * as yup from 'yup'
 
 const initialValues = {
@@ -51,6 +52,13 @@ const validationSchema = yup.object({
     isFree: isFreeValidator,
 })
 const CreateBarPage = () => {
+    const [isFreeValue, setIsFreeValue] = useState(initialValues.isFree)
+
+    const handleIsFreeChange = (e, formikProps) => {
+        const selectedValue = e.target.value
+        setIsFreeValue(selectedValue)
+        formikProps.setFieldValue('isFree', selectedValue)
+    }
     const handleSubmit = async (values, { resetForm }) => {
         await axios.post(
             'http://localhost:3000/api/places/create/museum',
@@ -69,89 +77,95 @@ const CreateBarPage = () => {
                 onSubmit={handleSubmit}
                 validationSchema={validationSchema}
             >
-                <Form className="flex justify-center">
-                    <p className="text-3xl m-4">Add a museum</p>
-                    <FormField
-                        className="w-80 text-neutral-950"
-                        name="name"
-                        placeholder="Type the place name ..."
-                        label="Place name"
-                    />
-                    <FormField
-                        className="w-80 text-neutral-950"
-                        name="address"
-                        placeholder="Type the address ..."
-                        label="Address"
-                    />
-                    <FormField
-                        className="w-80 text-neutral-950"
-                        name="city"
-                        placeholder="Type the city ..."
-                        label="City"
-                    />
-                    <FormField
-                        className="w-80 text-neutral-950"
-                        name="postalCode"
-                        placeholder="Type the Postal code ... "
-                        label="Postal Code"
-                    />
-                    <FormField
-                        className="w-80 text-neutral-950"
-                        name="country"
-                        placeholder="Type the country ..."
-                        label="Country"
-                    />
-                    <FormField
-                        className="w-80"
-                        as="select"
-                        name="artType"
-                        label="Art type"
-                    >
-                        {artType.map((artType) => (
-                            <option value={artType} key={artType}>
-                                {capitalizeFirstLetter(artType)}
-                            </option>
-                        ))}
-                    </FormField>
-                    <FormField
-                        className="w-80"
-                        as="select"
-                        name="artMovement"
-                        label="Art movement"
-                    >
-                        {artMovement.map((artMovement) => (
-                            <option value={artMovement} key={artMovement}>
-                                {capitalizeFirstLetter(artMovement)}
-                            </option>
-                        ))}
-                    </FormField>
-                    <FormField
-                        className="w-80"
-                        as="select"
-                        name="isFree"
-                        label="Free or Fee ?"
-                    >
-                        {isFree.map((isFree) => (
-                            <option value={isFree} key={isFree}>
-                                {capitalizeFirstLetter(isFree)}
-                            </option>
-                        ))}
-                    </FormField>
-                    <FormField
-                        className="w-80"
-                        as="select"
-                        name="avgPrice"
-                        placeholder=""
-                        label="Average price"
-                    >
-                        {avgPrice.map((avgPrice) => (
-                            <option value={avgPrice} key={avgPrice}>
-                                {capitalizeFirstLetter(avgPrice)}
-                            </option>
-                        ))}
-                    </FormField>
-                    <Button type="submit">Create</Button>
-                </Form>
+                {(formikProps) => (
+                    <Form className="flex justify-center">
+                        <p className="text-3xl m-4">Add a museum</p>
+                        <FormField
+                            className="w-80 text-neutral-950"
+                            name="name"
+                            placeholder="Type the place name ..."
+                            label="Place name"
+                        />
+                        <FormField
+                            className="w-80 text-neutral-950"
+                            name="address"
+                            placeholder="Type the address ..."
+                            label="Address"
+                        />
+                        <FormField
+                            className="w-80 text-neutral-950"
+                            name="city"
+                            placeholder="Type the city ..."
+                            label="City"
+                        />
+                        <FormField
+                            className="w-80 text-neutral-950"
+                            name="postalCode"
+                            placeholder="Type the Postal code ... "
+                            label="Postal Code"
+                        />
+                        <FormField
+                            className="w-80 text-neutral-950"
+                            name="country"
+                            placeholder="Type the country ..."
+                            label="Country"
+                        />
+                        <FormField
+                            className="w-80"
+                            as="select"
+                            name="artType"
+                            label="Art type"
+                        >
+                            {artType.map((artType) => (
+                                <option value={artType} key={artType}>
+                                    {capitalizeFirstLetter(artType)}
+                                </option>
+                            ))}
+                        </FormField>
+                        <FormField
+                            className="w-80"
+                            as="select"
+                            name="artMovement"
+                            label="Art movement"
+                        >
+                            {artMovement.map((artMovement) => (
+                                <option value={artMovement} key={artMovement}>
+                                    {capitalizeFirstLetter(artMovement)}
+                                </option>
+                            ))}
+                        </FormField>
+                        <FormField
+                            className="w-80"
+                            as="select"
+                            name="isFree"
+                            label="Free or Fee ?"
+                            onChange={(e) => handleIsFreeChange(e, formikProps)}
+                            value={formikProps.values.isFree}
+                        >
+                            {isFree.map((isFree) => (
+                                <option value={isFree} key={isFree}>
+                                    {capitalizeFirstLetter(isFree)}
+                                </option>
+                            ))}
+                        </FormField>
+                        {formikProps.values.isFree === isFree[0] ? null : (
+                            <FormField
+                                className="w-80"
+                                as="select"
+                                name="avgPrice"
+                                placeholder=""
+                                label="Average price"
+                            >
+                                {avgPrice.map((avgPrice) => (
+                                    <option value={avgPrice} key={avgPrice}>
+                                        {capitalizeFirstLetter(avgPrice)}
+                                    </option>
+                                ))}
+                            </FormField>
+                        )}
+                        <Button type="submit">Create</Button>
+                    </Form>
+                )}
             </Formik>
         </>
     )
